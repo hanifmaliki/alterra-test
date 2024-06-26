920f1ab8be7fed356d9309b7ebe34061b9c912dd
Hanif Maliki Dewanto <Hanif.Dewanto@petrosea.com>
2024-06-26 21:54:43 +0700

    Split the folder

---
+++ calculate/calculate.go
@@ -0,0 +1,28 @@
+package calculate
+
+import (
+	"slices"
+
+	"github.com/hanifmaliki/alterra-test/helper"
+)
+
+func CalculateCombinations(target, maxSum int) int {
+	numbers := make([]int, target)
+	for i := range numbers {
+		numbers[i] = 1
+	}
+	combinations := 1
+	helper.PrintIntSlice(numbers)
+	index := len(numbers) - 1
+
+	for len(numbers) > 1 && index > 0 {
+		if index > 0 && numbers[index]+numbers[index-1] <= maxSum {
+			numbers[index-1] += numbers[index]
+			numbers = slices.Delete(numbers, index, index+1)
+			combinations++
+			helper.PrintIntSlice(numbers)
+		}
+		index--
+	}
+	return combinations
+}
--- main.go
+++ main.go
@@ -2,33 +2,11 @@ package main
 
 import (
 	"fmt"
-	"slices"
 
-	"github.com/hanifmaliki/alterra-test/helper"
+	"github.com/hanifmaliki/alterra-test/calculate"
 )
 
-func CalculateCombinations(target, maxSum int) int {
-	numbers := make([]int, target)
-	for i := range numbers {
-		numbers[i] = 1
-	}
-	combinations := 1
-	helper.PrintIntSlice(numbers)
-	index := len(numbers) - 1
-
-	for len(numbers) > 1 && index > 0 {
-		if index > 0 && numbers[index]+numbers[index-1] <= maxSum {
-			numbers[index-1] += numbers[index]
-			numbers = slices.Delete(numbers, index, index+1)
-			combinations++
-			helper.PrintIntSlice(numbers)
-		}
-		index--
-	}
-	return combinations
-}
-
 func main() {
-	result := CalculateCombinations(6, 3)
+	result := calculate.CalculateCombinations(6, 3)
 	fmt.Println(result)
 }
